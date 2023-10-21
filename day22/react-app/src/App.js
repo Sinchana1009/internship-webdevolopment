a215484bb6d9b59f8f4bedc177dfcde1b7fd310f
Sinchana1009 <sinchram03@email.com>
2023-10-21 22:20:59 +0530

    done

--- day22/react-app/src/App.js
+++ day22/react-app/src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
-import Header from "./Components/Header";
-import Body from "./Components/Body";
-import Footer from "./Components/Footer"
+
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Navbar from  "./Components/Navbar";
+import Login from "./Pages/Login";
+import Signin from "./Pages/Signin";
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 const App=()=>{
   return(
     <>
-    <Header/>
-    <Body />
-    <Footer/>
-    </>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+    <Route element ={<About/>} path="/About"/>
+    <Route element ={<Contact/>} path="/Contact"/>
+    <Route element ={<Home/>} path="/Home"/>
+    <Route element ={<Signin/>} path="/Signin"/>
+    <Route element ={<Login/>} path="/Login"/>
+    </Routes>
+    </BrowserRouter>
+   </>
       );
 };
 
 export default App;
-
---
+++ day22/react-app/src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+function Home(){
+    return(
+        <h1 id="name" className="text-center mt-">Sinchana PR</h1>
+    );
+};
+export default Home;
