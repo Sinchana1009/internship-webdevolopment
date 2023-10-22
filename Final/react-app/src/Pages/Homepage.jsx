8838946e5f335f0f2ed8530ccf45991c626e10f9
Sinchana1009 <sinchram03@email.com>
2023-10-22 18:36:05 +0530

    done

---
+++ Final/react-app/src/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div className="container">
+      {/* Homepage content goes here */}
+    </div>
+  );
+}
+
+export default Homepage;
